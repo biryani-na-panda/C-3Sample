34d4ed4b143fffd0bdd31ab90b6945d9f992995b
biryani_na_panda <biryani.na.panda@gmail.com>
2024-03-01 20:04:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Example/Program.cs
@@ -0,0 +1,38 @@
+﻿/*List 3-1
+// 変数の宣言
+int money; // 所持金を代入する変数
+string name; // 名前を代入する変数
+
+// 変数に値を代入
+money = 5000;
+name = "きたむら";
+
+Console.WriteLine(money); // 所持金を表示
+Console.WriteLine(name); // 名前を表示
+*/
+
+/* List 3-2
+int answer; // 計算結果
+
+// 加算
+answer = 3 + 4;
+Console.WriteLine(answer);
+
+// 減算
+answer = 12 - 18;
+Console.WriteLine(answer);
+
+// 乗算
+answer = 2 * 7;
+Console.WriteLine(answer);
+
+// 除算
+answer = 18 / 3;
+Console.WriteLine(answer);
+
+// 剰除
+answer = 21 % 5;
+Console.WriteLine(answer);
+*/
+
+// List 3-3
